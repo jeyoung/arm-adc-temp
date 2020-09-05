dc5200817ec600091cbbfffeecc05e82de3ee972
Eddy Young <jeyoung@priscimon.com>
2020-09-05 15:43:54 +0100

    arm: Add and format comments

--- temp.c
+++ temp.c
@@ -1,45 +1,54 @@
 #include <math.h>
 #include "temp.h"
 
+/*
+ * This program reads the temperature from the onboard sensor and sends the
+ * output to USART1.
+ *
+ * The program waits for a key press from the console before sending the
+ * measured temperature value.
+ *
+ * The program is tested for an STM32 Nucleo F446RE.
+ */
 int main(void)
 {
-    // Enable GPIOA
+    /* Enable GPIOA */
     RCC_AHB1ENR |= (0x01 << 0x00);
-    // Alternate function (AF) mode for PA9 and PA10
+    /* Alternate function (AF) mode for PA9 and PA10 */
     GPIOA_MODER |= (0x02 << 0x12) | (0x02 << 0x14);
-    // AF7 (USART TX and RX) for PA9 and PA10
+    /* AF7 (USART TX and RX) for PA9 and PA10 */
     GPIOA_AFRH  |= (0x07 << 0x04) | (0x07 << 0x08); 
 
-    // Configure PA5 and PA6 for debugging output
+    /* Configure PA5 and PA6 for debugging output */
     GPIOA_MODER |= (0x01 << 0x0A) | (0x01 << 0x0C);
 
-    // Enable USART1
+    /* Enable USART1 */
     RCC_APB2ENR |= (0x01 << 0x04);
-    // Configure baud rate based on APB2 clock (equals to HSI 16MHz by default)
+    /* Configure baud rate based on APB2 clock (equals to HSI 16MHz by default) */
     USART1_BRR   = 0x683;
-    // Enable TX and RX, and USART overall
+    /* Enable TX and RX, and USART overall */
     USART1_CR1  |= (0x01 << 0x03) | (0x01 << 0x02) | (0x01 << 0x0D);
 
-    // Enable ADC1
+    /* Enable ADC1 */
     RCC_APB2ENR |= (0x01 << 0x08);
-    // Set channel 18 for temperature sensor
+    /* Set channel 18 for temperature sensor */
     ADC1_SQR3   |= (0x12 << 0x00);
-    // Set the number of conversion (which is same as default)
+    /* Set the number of conversion (which is same as default) */
     ADC1_SQR1   &= ~(0x0F << 0x14);
-    // Set sampling rate for channel 18 (must be > 10 for STM32 F446)
+    /* Set sampling rate for channel 18 (must be > 10 for STM32 F446) */
     ADC1_SMPR1  |= (0x01 << 0x18);
-    // Choose the temperature sensor for channel 18 (instead of battery)
+    /* Choose the temperature sensor for channel 18 (instead of battery) */
     ADC1_CCR    |= (0x01 << 0x17);
 
     while (1)
     {
-        // Turn ADC1 on
+        /* Turn ADC1 on */
         ADC1_CR2    |= (0x01 << 0x00);
 
-        // Start conversion
+        /* Start conversion */
         ADC1_CR2    |= (0x01 << 0x1E);
 
-        // Wait for conversion to complete
+        /* Wait for conversion to complete */
         while (!(ADC1_SR & (0x01 << 0x01)))
             ;
 
@@ -48,16 +57,16 @@ int main(void)
         float temp = ((vsense - V25)/AVG_SLOPE)+TEMP_OFFSET;
         if (temp > 0)
         {
-            // Light up PA5
+            /* Light up PA5 */
             GPIOA_ODR ^= (0x01 << 0x05);
         }
 
-        // Look for an RX
+        /* Look for an RX (wait for a keypress) */
         if (USART1_SR & (0x01 << 0x05))
         {
             volatile char char_in = USART1_DR;
 
-            // Toggle PA6
+            /* Toggle PA6 */
             GPIOA_ODR ^= (0x01 << 0x06);
 
             char output[OUTPUT_LENGTH];
@@ -86,7 +95,7 @@ int main(void)
 
             USART1_DR = '\t';
 
-            // Check for TC before continuing
+            /* Check for TC before continuing */
             while (!(USART1_SR & (0x01 << 0x06)))
                 ;
 
@@ -94,14 +103,14 @@ int main(void)
             {
                 USART1_DR = output[i];
 
-                // Check for TC before continuing
+                /* Check for TC before continuing */
                 while (!(USART1_SR & (0x01 << 0x06)))
                     ;
             }
 
         }
 
-        // Turn ADC1 off
+        /* Turn ADC1 off */
         ADC1_CR2    &= ~(0x01 << 0x00);
     }
 
